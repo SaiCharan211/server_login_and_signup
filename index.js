c8db30973763d4661b87e5b287716a4efd1883f2
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-25 12:55:50 +0530

    new

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app=express()
 app.use(express.json())
 app.use(cors(
     {
-        origin:["http://localhost:5173"],
+        origin:["https://frontend-jl452q8oz-poosalasaicharan123-gmailcoms-projects.vercel.app/"],
         credentials:true
     }
 ))
