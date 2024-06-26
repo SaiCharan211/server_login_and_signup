41a44aabcce755d60e6180889173bba8d811101c
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-26 10:25:07 +0530

    new

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const corsOptions={
 }
 app.use(cors(corsOptions))
 //app.use(cors())
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
-});
+});*/
 app.options('*', cors(corsOptions)); // This will handle preflight requests.
 
 app.use(cookieParser())
