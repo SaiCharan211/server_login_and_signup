4756fbeaa0f347b159a40544be8e0f254063a1ab
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-26 12:51:32 +0530

    new

--- index.js
+++ index.js
@@ -9,28 +9,16 @@ import { UserRouter } from './componets/route.js';
 
 const app=express()
 app.use(express.json())
-/* app.use(cors({
-    origin: 'http://localhost:5173',
+
+app.use(cors({
+    origin: 'https://client-login-and-signup.vercel.app/',
     credentials:true
 }))
 app.options('*', cors());
 app.use(cookieParser())
 app.use('/',UserRouter)
-*/
-const allowedOrigins = ["http://localhost:5173", "https://client-login-and-signup.vercel.app"];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-};
+
 
 app.use(cors(corsOptions));
 app.options('*', cors());
