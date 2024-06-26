4542c9f34c229f16c19d0fe02a95faf81cbcffd9
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-26 13:51:49 +0530

    new

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const allowedOrigins = ["http://localhost:5173", "https://client-login-and-signup.vercel.app"];
+// CORS middleware
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin:"https://client-login-and-signup.vercel.app",
   credentials: true
 };
 
@@ -29,10 +29,7 @@ app.use(cors(corsOptions));
 app.options('*', cors());
 
 app.use(cookieParser());
-app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.url}`);
-  next();
-});
+
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Connected to MongoDB'))
