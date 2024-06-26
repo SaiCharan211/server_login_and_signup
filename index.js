5a6eae0312884f91fb28f6f6484d50758cb10a37
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-26 13:58:03 +0530

    new

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import jwt from 'jsonwebtoken'; // Ensure you have this package installed
-import bcrypt from 'bcrypt'; // Ensure you have this package installed
 
-dotenv.config();
+dotenv.config() 
+import { UserRouter } from './componets/route.js';
 
-const app = express();
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app=express()
+app.use(express.json())
+app.use(cors(
+    {
+        origin:["http://localhost:5173","https://client-login-and-signup.vercel.app"],
+        credentials:true
+    }
+))
+app.use(cookieParser())
+app.use('/auth',UserRouter)
 
-// CORS middleware
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+mongoose.connect("mongodb://localhost:27017/authentication")
 
-const corsOptions = {
-  origin:"https://client-login-and-signup.vercel.app",
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors());
-
-app.use(cookieParser());
-
-
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB', err));
-
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT,()=>{
+    console.log('Server is running')
+})
