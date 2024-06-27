941fa12d619d9afea7823325c55ca099a133242a
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-27 12:07:09 +0530

    new

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ import { UserRouter } from './componets/route.js';
 
 const app=express()
 app.use(express.json())
-app.use(cors(
-    {
-        origin:["http://localhost:5173"],
-        credentials:true
-    }
-))
+app.use(cors())
 app.use(cookieParser())
 app.use('/',UserRouter)
 
