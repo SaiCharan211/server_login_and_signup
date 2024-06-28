3b50c3462c1df44994c424475797714a314254ad
Sai Charan Poosala <saicharan9071@gmail.com>
2024-06-28 10:52:38 +0530

    new

--- componets/route.js
+++ componets/route.js
@@ -79,7 +79,7 @@ router.post('/forgot-password',async(req,res)=>{
       from: 'saicharan9071@gmail.com',
       to:email,
       subject: 'Reset Password',
-      text:`https://client-login-and-signup.onrender.com/resetPassword/${token}`
+      text:` http://localhost:5173/resetPassword/${token}`
     };
     
     transporter.sendMail(mailOptions, function(error, info){
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { UserRouter } from './componets/route.js';
 const app=express()
 app.use(express.json())
 app.use(cors({
-    origin: 'https://client-login-and-signup.onrender.com',
+    origin: ' http://localhost:5173/',
     credentials: true,
 }))
 app.use(cookieParser())
